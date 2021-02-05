f15e22f551d8647e3d583c6a2dd10e1b6a4898c5
dylan275 <dylan.motard@etu.univ-tours.fr>
2021-02-05 10:50:22 +0100

    Spring Boot project

---
+++ src/main/java/fr/univ_tours/polytech/pet_clinic_manager_back/PetClinicManagerBackApplication.java
@@ -0,0 +1,13 @@
+package fr.univ_tours.polytech.pet_clinic_manager_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univ_tours/polytech/pet_clinic_manager_back/PetClinicManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ_tours.polytech.pet_clinic_manager_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
