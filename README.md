980e38f14e51a5f280660fe2925a3c1ea053c582
dylan275 <dylan.motard@etu.univ-tours.fr>
2021-03-26 11:30:56 +0100

    End project

--- README.md
+++ README.md
@@ -1,38 +1,44 @@
 # Application Web de Gestion des Animaux d'une Clinique Vétérinaire
 
-## Projet Libre S10
+## Projet Libre S10 - Partie Back-end
 
 <em>Auteurs : Dylan MOTARD & Florian GIGOT </em>
+
+## Installation
+
+* Installer MySQL et déployer la base de données pet_clinic_manager.sql qui se trouve à la racine du projet
+* Installer Apache Maven
+
+```shell
+git clone https://github.com/florianG37/Pet_Clinic_Manager_back.git
+cd Pet_Clinic_Manager_back
+mvn spring-boot:run
+```
+
+* Accéder à Swagger depuis http://localhost:8080/swagger-ui.html
+
+## Partie Front-end
+
+Dépôt : https://github.com/florianG37/Pet_Clinic_Manager_front
 
 ## Technologies
 
 Pour ce projet, nous allons utiliser les technologies suivantes :
+
 * Le framework Angular pour la partie front-end du projet
 * Le framework Spring pour la partie back-end du projet réalisée en Java
 * MySQL pour la base de données
 
-
 ## Description
 
-Cette application se base sur 2 grands axes : 
-* La gestion générale des flux des animaux dans une clinique vétérinaire.
-* La visualisation de statistiques générales pour la prise de décision.
-
 Gestion des flux :
+
 * Gestion des fiches d’animaux (identifiant, nom, date de naissance, propriétaire, type, race)
-* Gestion des fiches de propriétaires d’animaux (identifiant, prénom, nom, adresse, ville, téléphone, courriel)
-* Gestion des fiches de vétérinaires (identifiant, prénom, nom, adresse, ville, téléphone, courriel, salaire, temps de travail, type de contrat)
-* Gestion des types d’animaux (identifiant, nom, races)
-* Gestion des races d’animaux (identifiant, nom)
+* Gestion des fiches de vétérinaires (identifiant, prénom, nom, adresse, ville, téléphone, courriel, salaire, temps de
+  travail, type de contrat)
 * Gestion des visites (identifiant, date d’entrée, date de sortie, animal, vétérinaire)
 
-Visualisation des statistiques :
-* Nombre de visites en fonction du temps (Courbe)
-* Nombre d’animaux en fonction du type d’animal (Histogramme)
-* Temps moyen par visite (Nombre)
-* Nombre moyen d’animaux par propriétaire (Nombre)
-* Age moyen des animaux (Nombre)
-* Heures supplémentaires des vétérinaires (Pourcentage)
-
 Gestion des utilisateurs :
+
+* Gestion de l'authentification par un token Bearer JWT
 * Gestion des utilisateurs par l’administrateur
--- pet_clinic_manager.sql
+++ pet_clinic_manager.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 06 fév. 2021 à 15:02
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Généré le : ven. 26 mars 2021 à 10:30
+-- Version du serveur :  8.0.22
+-- Version de PHP : 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,89 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `pet_clinic_manager`
+-- Base de données : `pet_clinic_manager`
 --
 CREATE DATABASE IF NOT EXISTS `pet_clinic_manager` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `pet_clinic_manager`;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `breed`
---
-
-DROP TABLE IF EXISTS `breed`;
-CREATE TABLE IF NOT EXISTS `breed` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `idType` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idType` (`idType`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `breed`
---
-
-INSERT INTO `breed` (`id`, `name`, `idType`) VALUES
-(1, 'Berger allemand', 1),
-(2, 'Bulldog anglais', 1),
-(3, 'Caniche', 1),
-(4, 'Labrador', 1),
-(5, 'Golden retriever', 1),
-(6, 'Maine coon', 2),
-(7, 'British shorthair', 2),
-(8, 'Pur-sang', 5),
-(9, 'Arabe', 5);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `owner`
---
-
-DROP TABLE IF EXISTS `owner`;
-CREATE TABLE IF NOT EXISTS `owner` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `idPerson` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idPerson` (`idPerson`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `owner`
---
-
-INSERT INTO `owner` (`id`, `idPerson`) VALUES
-(1, 1),
-(2, 3);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `person`
---
-
-DROP TABLE IF EXISTS `person`;
-CREATE TABLE IF NOT EXISTS `person` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `adress` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `phoneNumber` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `person`
---
-
-INSERT INTO `person` (`id`, `firstname`, `lastname`, `adress`, `city`, `phoneNumber`, `email`) VALUES
-(1, 'Patrick', 'Dupond', '7 rue des Patricks', 'Tours', '0655555555', 'patrick.dupond@mail.fr'),
-(2, 'Jean', 'Defienas', '8 rue nationale', 'Tours', '0788888888', 'jean.defienas@mail.com'),
-(3, 'Bernard', 'Loutre', '99 rue de Tours', 'Tours', '0247855858', 'bernard.loutre@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -111,94 +31,79 @@ INSERT INTO `person` (`id`, `firstname`, `lastname`, `adress`, `city`, `phoneNum
 
 DROP TABLE IF EXISTS `pet`;
 CREATE TABLE IF NOT EXISTS `pet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `dateBirth` date NOT NULL,
-  `gender` varchar(255) NOT NULL,
-  `idType` int(11) NOT NULL,
-  `idOwner` int(11) NOT NULL,
-  `idVet` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idType` (`idType`,`idOwner`,`idVet`),
-  KEY `idOwner` (`idOwner`),
-  KEY `pet_ibfk_2` (`idVet`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `pet`
---
-
-INSERT INTO `pet` (`id`, `name`, `dateBirth`, `gender`, `idType`, `idOwner`, `idVet`) VALUES
-(1, 'Cacahouete', '2021-02-03', 'Male', 5, 1, 1),
-(2, 'Garfield', '2020-05-08', 'Male', 2, 1, 1),
-(3, 'Chanel', '2016-04-04', 'Femelle', 1, 2, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `role`
---
-
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE IF NOT EXISTS `role` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `datebirth` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `owner` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `breed` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `role`
---
-
-INSERT INTO `role` (`id`, `name`) VALUES
-(0, 'ADMIN'),
-(1, 'USER');
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `type`
+-- Structure de la table `roles`
 --
 
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE IF NOT EXISTS `type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `type`
---
-
-INSERT INTO `type` (`id`, `name`) VALUES
-(1, 'Chien'),
-(2, 'Chat'),
-(5, 'Cheval');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `idRole` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id` (`idRole`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `user`
+-- Déchargement des données de la table `roles`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `idRole`) VALUES
-(1, 'admin', 'admin', 0),
-(2, 'user1', 'user', 1);
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'ROLE_USER'),
+(2, 'ROLE_ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `password` varchar(120) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `password`, `username`) VALUES
+(1, '$2a$10$5Lo/D2nivQEQdsF9Ds0Ht.q6qFAIUu2HSZkUJrYseZm.F4u/NwGmq', 'Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `user_id` bigint NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FKh8ciramu9cc9q3qcqiv4ue8a6` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -208,21 +113,13 @@ INSERT INTO `user` (`id`, `username`, `password`, `idRole`) VALUES
 
 DROP TABLE IF EXISTS `vet`;
 CREATE TABLE IF NOT EXISTS `vet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `salary` int(11) NOT NULL,
-  `workTime` int(11) NOT NULL,
-  `typeEmploymentContrat` varchar(255) NOT NULL,
-  `idPerson` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idPerson` (`idPerson`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `vet`
---
-
-INSERT INTO `vet` (`id`, `salary`, `workTime`, `typeEmploymentContrat`, `idPerson`) VALUES
-(1, 50000, 39, 'CDI', 2);
+  `id` int NOT NULL AUTO_INCREMENT,
+  `salary` int DEFAULT NULL,
+  `work_time` int DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -232,67 +129,35 @@ INSERT INTO `vet` (`id`, `salary`, `workTime`, `typeEmploymentContrat`, `idPerso
 
 DROP TABLE IF EXISTS `visit`;
 CREATE TABLE IF NOT EXISTS `visit` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `dateEntry` date NOT NULL,
-  `dateLeaving` date NOT NULL,
-  `description` text NOT NULL,
-  `petWeight` int(11) NOT NULL,
-  `idVet` int(11) NOT NULL,
-  `idPet` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_entry` varchar(255) DEFAULT NULL,
+  `date_leaving` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `pet_weight` int DEFAULT NULL,
+  `id_pet` int DEFAULT NULL,
+  `id_vet` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `idVet` (`idVet`,`idPet`),
-  KEY `idPet` (`idPet`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `visit`
---
-
-INSERT INTO `visit` (`id`, `dateEntry`, `dateLeaving`, `description`, `petWeight`, `idVet`, `idPet`) VALUES
-(1, '2021-02-06', '2021-02-07', 'Opération de la hanche avant gauche', 15, 1, 2);
+  KEY `FKooohregrhu964bmpdpmf5ssjy` (`id_pet`),
+  KEY `FKnfyo5iep8jvrq0rw7qh4axtft` (`id_vet`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `breed`
+-- Contraintes pour la table `user_roles`
 --
-ALTER TABLE `breed`
-  ADD CONSTRAINT `breed_ibfk_1` FOREIGN KEY (`idType`) REFERENCES `type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `owner`
---
-ALTER TABLE `owner`
-  ADD CONSTRAINT `owner_ibfk_1` FOREIGN KEY (`idPerson`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `pet`
---
-ALTER TABLE `pet`
-  ADD CONSTRAINT `pet_ibfk_1` FOREIGN KEY (`idType`) REFERENCES `type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pet_ibfk_2` FOREIGN KEY (`idVet`) REFERENCES `vet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pet_ibfk_3` FOREIGN KEY (`idOwner`) REFERENCES `owner` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idRole`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `vet`
---
-ALTER TABLE `vet`
-  ADD CONSTRAINT `vet_ibfk_1` FOREIGN KEY (`idPerson`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `FKh8ciramu9cc9q3qcqiv4ue8a6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  ADD CONSTRAINT `FKhfh9dx7w3ubf1co1vdev94g3f` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Contraintes pour la table `visit`
 --
 ALTER TABLE `visit`
-  ADD CONSTRAINT `visit_ibfk_1` FOREIGN KEY (`idPet`) REFERENCES `pet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `visit_ibfk_2` FOREIGN KEY (`idVet`) REFERENCES `vet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FKnfyo5iep8jvrq0rw7qh4axtft` FOREIGN KEY (`id_vet`) REFERENCES `vet` (`id`),
+  ADD CONSTRAINT `FKooohregrhu964bmpdpmf5ssjy` FOREIGN KEY (`id_pet`) REFERENCES `pet` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
